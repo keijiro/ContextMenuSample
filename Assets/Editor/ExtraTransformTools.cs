1c190281ed407138317352c74730e99734510750
Keijiro Takahashi <keijiro@gmail.com>
2024-02-22 15:44:54 +0900

    initial commit

---
+++ Assets/Editor/ExtraTransformTools.cs
@@ -0,0 +1,21 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class ExtraTransformTools
+{
+    [MenuItem("CONTEXT/Transform/Reset Rotation")]
+    static void ResetRotation(MenuCommand command)
+    {
+        Undo.RecordObjects(Selection.transforms, "Reset Rotation");
+        foreach (var xform in Selection.transforms)
+            xform.localRotation = Quaternion.identity;
+    }
+
+    [MenuItem("CONTEXT/Transform/Randomize Rotation")]
+    static void RandomizeRotation(MenuCommand command)
+    {
+        Undo.RecordObjects(Selection.transforms, "Randomize Rotation");
+        foreach (var xform in Selection.transforms)
+            xform.localRotation = Random.rotation;
+    }
+}
